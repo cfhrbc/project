aaee3111bc1a6075f14370cf3d29d4083e3a1191
cfhrbc <serarmynin@bk.ru>
2025-03-25 15:51:41 +0400

    zadacha #6 вроде бы готово.

--- src/main/java/ru/kata/spring/boot_security/demo/mapper/RoleMapper.java
+++ src/main/java/ru/kata/spring/boot_security/demo/mapper/RoleMapper.java
@@ -7,7 +7,7 @@ import ru.kata.spring.boot_security.demo.model.RoleDto;
 @Mapper(componentModel = "spring")
 public interface RoleMapper {
     RoleDto toDto(Role role);
-
     Role toEntity(RoleDto roleDto);
+
 
 }
